cff95c158a8f60b2b07f825ce1d8204cb9537a57
葛晨晨 <gechenchen@peilian.com>
2020-03-09 23:32:02 +0800

    fix

---
+++ admin/js/index.js
@@ -0,0 +1,17 @@
+
+/*
+$(document).ready(function() {//时间设置
+    function currentTime() {
+        var d = new Date(), str = '';
+        str += d.getFullYear() + '年';
+        str += d.getMonth() + 1 + '月';
+        str += d.getDate() + '日';
+        str += d.getHours() + '时';
+        str += d.getMinutes() + '分';
+        str += d.getSeconds() + '秒';
+        return str;
+    }
+    setInterval(function () {
+        $('#time').html(currentTime)
+    }, 1000);
+});*/
