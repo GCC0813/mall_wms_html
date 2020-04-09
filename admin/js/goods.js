5a82311cce4e2cdb07441948b3b6721a405ddc7e
葛晨晨 <gechenchen@peilian.com>
2020-04-09 23:27:01 +0800

    fix

--- admin/js/goods.js
+++ admin/js/goods.js
@@ -39,8 +39,17 @@ function initialization(goodsId,loadIndex,index){
                 goods_supplier.attr('placeholder',data.supplier);
                 physical_or_virtual.attr('placeholder',data.physicalOrVirtual);
             }else if(index===2){
-                $.post(domain+'/goods/details',objToJson(jsonReq),function (res) {
-
+                let o ={
+                    categoryId:data.categoryId,
+                    tagId:data.tagId
+                };
+                $.post(domain+'/goods/details/goodsVariety',objToJson(o),function (resp) {
+                    if(resp.code===200){
+                        set_category_tag(resp.data.rows);
+                    }else {
+                        $("#goods_category_select").html('<option value="0">直接选择或搜索选择分类</option>');
+                        $("#goods_tag_select").html('<option value="0">直接选择或搜索选择标签</option>');
+                    }
                 });
             }
             $('#market-price').attr('placeholder','￥'+data.marketPrice);
@@ -72,4 +81,51 @@ function initialization(goodsId,loadIndex,index){
             layer.msg(res.msg)
         }
     });
+}
+function set_category_tag(goodVrietyList) {
+    let form = layui.form;
+    let $ = layui.$;
+    let goodVriety = "";
+    let goodVrietySelectStr = '';
+    let goods_category_select = $("#goods_category_select");
+    for (let i = 0, len = goodVrietyList.length; i < len; i++) {
+        goodVriety = goodVrietyList[i];
+        goodVrietySelectStr +="<option value='" + goodVriety.vrietyId + "'>" + goodVriety.vrietyName + "</option>"
+    }
+    goods_category_select.html(goodVrietySelectStr);
+
+    let selectGoodVriety = goods_category_select.val();
+
+    let goodTagsList="";
+    for (let i in goodVrietyList){
+        if(goodVrietyList[i].vrietyId === parseInt(selectGoodVriety)){
+            goodTagsList = goodVrietyList[i].goodTagsList
+        }
+    }
+    let goodTags = "";
+    let goodTagsSelectStr = "";
+    for (let i = 0; i < goodTagsList.length; i++) {
+        goodTags = goodTagsList[i];
+        goodTagsSelectStr += "<option value='" + goodTags.tagId + "'>" + goodTags.tagName + "</option>"
+    }
+    $("#goods_tag_select").html(goodTagsSelectStr);
+    form.on('select(check_on_tag)', function(data){
+        let obj={
+            categoryId : data.value
+        };
+        $.post(domain+'/goods/details/get_tags_by_cate',objToJson(obj),function (resp) {
+            if (resp.code === 200) {
+                let goodTagsList = resp.data.rows;
+                let tag = "";
+                $.each(goodTagsList,function (index, data) {
+                    tag += '<option value="'+data.tagId+'">'+data.tagName+'</option>';
+                });
+                $("#goods_tag_select").html(tag);
+                form.render('select');
+            } else {
+                $("#goods_tag_select").html('<option value="0">直接选择或搜索选择标签</option>');
+            }
+        });
+    });
+    form.render('select');
 }
